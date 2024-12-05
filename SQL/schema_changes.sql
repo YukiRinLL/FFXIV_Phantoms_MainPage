6cb110418eb46a66029fdabc66937d6990bc9944
YukiRinLL <944989026@qq.com>
2024-12-05 16:28:51 +0800

    user register refactoring

--- SQL/schema_changes.sql
+++ SQL/schema_changes.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
 ALTER TABLE users ADD COLUMN email VARCHAR(255) UNIQUE;
 ALTER TABLE users ADD COLUMN user_id UUID;
 ALTER TABLE users ADD CONSTRAINT users_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id);
+ALTER TABLE users ALTER COLUMN user_id SET DEFAULT auth.uid();
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
@@ -19,6 +20,7 @@ ALTER TABLE messages ADD COLUMN user_id UUID;
 ALTER TABLE messages DROP CONSTRAINT messages_user_id_fkey;
 ALTER TABLE messages ADD CONSTRAINT messages_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id);
 ALTER TABLE messages ADD CONSTRAINT messages_legacy_user_id_fkey FOREIGN KEY (legacy_user_id) REFERENCES users(id);
+ALTER TABLE messages ALTER COLUMN user_id SET DEFAULT auth.uid();
 
 
 CREATE TABLE images (
@@ -42,6 +44,7 @@ ALTER TABLE user_profile RENAME COLUMN user_id TO legacy_user_id;
 ALTER TABLE user_profile ADD COLUMN user_id UUID;
 ALTER TABLE user_profile ADD CONSTRAINT user_profile_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id);
 ALTER TABLE user_profile ADD CONSTRAINT user_profile_legacy_user_id_fkey FOREIGN KEY (legacy_user_id) REFERENCES users(id);
+ALTER TABLE user_profile ALTER COLUMN user_id SET DEFAULT auth.uid();
 
 
 
