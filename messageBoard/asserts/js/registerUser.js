f0c4e314a5f4ba492cc6eb65f35a8bd21bad8373
YukiRinLL <944989026@qq.com>
2025-08-01 16:29:03 +0800

    fix bug

--- messageBoard/asserts/js/registerUser.js
+++ messageBoard/asserts/js/registerUser.js
@@ -48,7 +48,8 @@ function sendSignupRequest(email, password) {
     .then(function(data) { // 这里处理解析后的JSON对象
         //console.log(data);
         const userToken = data.access_token;
-        const userId = data.id;
+        //const userId = data.id; // Phantoms_DB 使用的json格式
+        const userId = data.user.id // Phantoms_DB_Reboot 使用的json格式
         document.cookie = `access_token=${userToken}; path=/; secure;`;
         document.cookie = `user_id=${userId}; path=/; secure;`;
         console.log('Cookie has been set. Cookie content:', getCookie('access_token'), getCookie('user_id')); // 打印cookie内容
